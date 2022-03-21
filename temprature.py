def0fea526cf85256c5370aa8cfb31054640be7f
yasin <yasinnyilmazz26@gmail.com>
2022-03-21 11:57:09 +0300

    temprature question added to github

---
+++ temprature.py
@@ -0,0 +1,5 @@
+#Write a short Python program that asks the user to enter Celsius temperature (it can be a decimal number), 
+#converts the entered temperature into Fahrenheit degree and prints the result.
+
+temperature=int(input("Please enter a Celcius temprature "))
+print("Your temperature= " + str(temperature*33.8) + " Fahrenheit")
